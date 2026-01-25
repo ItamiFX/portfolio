f89c8ad1f9c82da3f11b18483865bb48a7f19602
Killian <killian.marquet@etu.u-bordeaux.fr>
2026-01-25 14:25:01 +0100

    chg pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import photo from '/assets/killian.jpg'
+import photo from '/assets/killian.png'
 
 export default function About(){
   return (
